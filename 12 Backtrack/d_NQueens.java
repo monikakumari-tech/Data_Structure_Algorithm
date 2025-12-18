49718b5c44e4913574c7584ccbd3c014b80377a5
Monika18092023 <mk.18092023@gmail.com>
2025-12-18 15:43:44 +0530

    gridways

--- 12 Backtrack/d_NQueens.java
+++ 12 Backtrack/d_NQueens.java
@@ -1,3 +1,77 @@
 public class d_NQueens {
+    public static boolean isSafe(char board[][], int row, int col) {
+        // vertical up
+        for (int i = row - 1; i >= 0; i--) {
+            if (board[i][col] == 'Q') {
+                return false;
+            }
+        }
+        // diagonal left up
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+        // diagonal right up
+        for (int i = row - 1, j = col + 1; i >= 0 && j < board.length; i--, j++) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+        return true;
+    }
 
+    public static boolean solveNQueens(char board[][], int row) {
+        // base case
+        if (row == board.length) {
+            count++;
+
+            // print the board
+
+            // printBoard(board);
+            return true;
+        }
+
+        // placing the queens and checking for each column
+        for (int j = 0; j < board.length; j++) {
+            if (isSafe(board, row, j)) {
+                board[row][j] = 'Q'; // place queen
+                if (solveNQueens(board, row + 1)) {
+                    return true;
+                } // recursive call
+                board[row][j] = 'x'; // backtrack
+            }
+        }
+        return false;
+    }
+
+    static int count = 0;
+
+    public static void printBoard(char board[][]) {
+        System.out.print("----possible solution----\n");
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println("");
+        }
+    }
+
+    public static void main(String args[]) {
+        int n = 5; // Example for 4-Queens
+        char[][] board = new char[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = 'x';
+            }
+        }
+        if (solveNQueens(board, 0)) {
+            printBoard(board);
+        } else {
+            System.out.println("Solution not possible");
+        }
+        ;
+        System.out.println("Total solutions: " + count);
+
+    }
 }
--- 12 Backtrack/e_gridways.java
+++ 12 Backtrack/e_gridways.java
@@ -1,3 +1,22 @@
 public class e_gridways {
+    public static int gridWays(int i, int j, int n, int m) {
+        // base case
+        if (i == n - 1 && j == m - 1) {
+            return 1;
+        } else if (i == n || j == m) {
+            return 0;
+        }
 
+        // move down
+        int downWays = gridWays(i + 1, j, n, m);
+        // move right
+        int rightWays = gridWays(i, j + 1, n, m);
+
+        return downWays + rightWays;
+    }
+
+    public static void main(String args[]) {
+        int n = 3, m = 4;
+        System.out.println("Total ways" + gridWays(0, 0, n, m));
+    }
 }
