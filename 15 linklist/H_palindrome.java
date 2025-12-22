da5fdc392e3890f83ea7aa01cdcdc83947f6252c
Monika18092023 <mk.18092023@gmail.com>
2025-12-22 17:01:29 +0530

    palindrome

--- 15 linklist/H_palindrome.java
+++ 15 linklist/H_palindrome.java
@@ -1,3 +1,111 @@
 public class H_palindrome {
+    public static class Node {
+        int data;
+        Node next;
 
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+
+    }
+
+    public static Node head;
+    public static Node tail;
+
+    public void addFirst(int data) {
+        // create new node
+        Node newNode = new Node(data);
+        if (head == null) {
+
+            head = tail = newNode;
+            return;
+        }
+        // newNode next= head
+        newNode.next = head;
+        // head= newNode
+        head = newNode;
+    }
+
+    public void addLast(int data) {
+        // create new node
+        Node newNode = new Node(data);
+        if (head == null) {
+            newNode = head = tail;
+            return;
+        }
+        // tail's next point to newnode
+        tail.next = newNode;
+        // update tail to newnode
+        tail = newNode;
+    }
+
+    public void print() {
+        if (head == null) {
+            System.out.println("LL is empty");
+            return;
+        }
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public Node findMid(Node head) {
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    public boolean isPalindrome() {
+        if (head == null || head.next == null) {
+            return true;
+        }
+
+        // step1: find mid
+        Node midNode = findMid(head);
+
+        // step2: reverse 2nd half
+        Node prev = null;
+        Node curr = midNode;
+        Node next;
+
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        // step3: compare both halves
+        Node right = prev; // head of 2nd half
+        Node left = head;
+
+        while (right != null) {
+            if (left.data != right.data) {
+                return false;
+            }
+            left = left.next;
+            right = right.next;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        H_palindrome ll = new H_palindrome();
+        ll.addFirst(2);
+        ll.addFirst(3);
+        ll.addLast(2);
+        ll.addLast(3);
+
+        ll.print();
+        System.out.println(ll.isPalindrome());
+    }
 }
